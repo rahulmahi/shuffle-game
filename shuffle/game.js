19464e83cef862071517421f9a2991d3289eadc9
rahul <rahul.valasai@gmail.com>
2022-07-29 15:21:47 +0530

    shuffle-game.

--- shuffle/game.js
+++ shuffle/game.js
@@ -4,9 +4,15 @@ let btn = document.querySelector('#btn');
 
 btn.addEventListener('click', only);
 
+var attempts = document.querySelector('#attempt');
+
+var attempt = 0;
+
 function only() {
+
+    attempt += 1;
     empty_arr = [];
-    let arr = ['H','M','I','L','R','G','U','A'];
+    let arr = ['H', 'M', 'E', 'L', 'R', 'G', 'U', 'A'];
 
     for (let i = 0; i < arr.length; i++) {
 
@@ -20,6 +26,48 @@ function only() {
         box_1[i].innerHTML = empty_arr[i];
         //console.log(empty_arr);
     }
-
-
 }
+
+var btn_click = document.querySelector('#btn-click');
+
+var val = document.querySelector('#input-box-val');
+
+var arr = ['large', 'merge', 'leg', 'real', 'hema', 'mega', 'ug', 'age', 'me'];
+
+btn_click.addEventListener('click', only1);
+
+var points_val = document.querySelector('#points');
+
+var points = 0;
+
+var array = [];
+
+function only1() {
+    var input_box = document.querySelector('#input-box1');
+    val_box = input_box.value;
+    for (let i = 0; i < arr.length; i++) {
+        if (!(array.includes(arr[i]))) {
+            if (val_box == arr[i]) {
+                val.innerHTML += val_box + "<br>";
+                array.push(val_box);
+                console.log(array);
+                points += 2;
+                points_val.textContent = 'SCORE : ' + points;
+            }
+        }
+    }
+    input_box.value = "";
+    console.log(input_box);
+}
+
+var over_btn = document.querySelector('#over-btn');
+
+var scores = document.querySelector('#score');
+
+over_btn.addEventListener('click',only2)
+
+    function only2(){
+        attempts.textContent = 'ATTEMPT : ' + attempt;
+        scores.textContent = 'SCORE : ' + points;
+        array = [];
+    }
